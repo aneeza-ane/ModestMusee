df68dc903c48723fd71c98532fe58fb87eb4b6a2
aneeza-ane <anyzzafarooq@gmail.com>
2025-12-12 11:27:41 +0500

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
-import { ProductProvider } from './Admin/Context/ProductContext.jsx';
+import { ProductProvider } from './Admin/Context/ProductContext';
 
 
 
