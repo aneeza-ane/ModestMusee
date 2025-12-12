e4fd8ddb23ea63b2a58669e0d8b998263cc3d80a
aneeza-ane <anyzzafarooq@gmail.com>
2025-12-12 11:26:22 +0500

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
-
-
 import { ProductProvider } from './Admin/Context/ProductContext.jsx';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
