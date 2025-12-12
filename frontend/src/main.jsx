3ce1f12bcbe47a833eb1ca71e11075cd2fd7793d
aneeza-ane <anyzzafarooq@gmail.com>
2025-12-12 10:51:02 +0500

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 
 
-import { ProductProvider } from './Admin/Context/ProductContext.jsx';
+import { ProductProvider } from './Admin/Context/ProductContext.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
